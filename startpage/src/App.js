45bc5d1e4e9a3b920f9bff90ae064d347375c1b2
derek-ye <37666121+derek-ye@users.noreply.github.com>
2020-04-02 22:49:34 -0700

    Add login form and spinning peter

--- startpage/src/App.js
+++ startpage/src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          
+        <div class="half">
+          <div class="login-box">
+            <div class="title">
+              <h1>Hack UCI Application</h1>
+            </div>
+            <div class="form-label">
+              <h2>Name</h2>
+              <input type="text" placeholder="Name"></input>
+            </div>
+            <div class="form-label">
+              <h2>Email</h2>
+              <input type="text" placeholder="Email"></input>
+            </div>
+            <div class="name">
+              <h2>Name</h2>
+              <input type="text" placeholder="Name"></input>
+            </div>
+
+          </div>
         </div>
-        <div>
-          <img src={logo} className="App-logo" alt="logo" />
+        <div class="half">
+          <img src="petr.png" className="App-logo" alt="logo" />
         </div>
       </header>
     </div>

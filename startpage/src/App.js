ef61056ac45c4c9f446bc789f8913259df98e622
derek-ye <37666121+derek-ye@users.noreply.github.com>
2020-04-03 20:33:46 -0700

    Added errors

--- startpage/src/App.js
+++ startpage/src/App.js
@@ -3,15 +3,71 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  // const txtFieldState = {
-  //   value: "",
-  //   valid: true,
-  //   errMsg: "" //this is where our error message gets across
-  // };
-  
+  state = {
+    name: "",
+    email: "",
+    fact: "",
+    nameError: "",
+    emailError: "",
+    factError: ""
+  }
 
+  editName = (e) => {
+    this.setState({name: e.target.value});
+    this.setState({nameError: ""});
+    console.log(e.target.value);
+  }
 
-  // document.addEventListener('submit-button', submit);
+  editEmail = (e) => {
+    this.setState({email: e.target.value});
+    this.setState({emailError: ""});
+    console.log(e.target.value);
+  }
+
+  editFact = (e) => {
+    this.setState({fact: e.target.value});
+    this.setState({factError: ""});
+    console.log(e.target.value);
+  }
+
+  submit = (e) => {
+    if (this.isProblem()) {
+
+    }
+  }
+
+  isProblem = () => {
+    // check name
+    if (this.state.name.length === 0) {
+      this.setState({nameError: "Please enter a name"});
+      return false;
+    }
+    // check emails
+    if (this.state.email === "") {
+      this.setState({emailError: "Please enter an email"});
+      return false;
+    }
+    else if (this.state.email.length < 7) {
+      this.setState({emailError: "Email is too short"});
+      return false;
+    }
+    else if (this.state.email.slice(-4) !== ".com") {
+      this.setState({emailError: "Email must be at a valid website"});
+      return false;
+    }
+    else if (!this.state.email.includes("@")) {
+      this.setState({emailError: "Not an email"});
+      return false;
+    }
+    // check fact
+    if (this.state.fact.length === 0) {
+      this.setState({factError: "Please enter a fact"});
+      return false;
+    }
+
+    return true;
+  }
+
   render() {
     return (
     <div className="App">
@@ -23,18 +79,20 @@ class App extends Component {
             </div>
             <div className="form-label">
               <h2>Name</h2>
-              <input type="text" placeholder="Name"></input>
+              <h3>{this.state.nameError}</h3>
+              <input type="text" placeholder="Name" value={this.state.name} onChange={this.editName}></input>
             </div>
             <div className="form-label">
               <h2>Email</h2>
-              <input type="text" placeholder="Email"></input>
+              <h3>{this.state.emailError}</h3>
+              <input type="text" placeholder="Email" value={this.state.email} onChange={this.editEmail}></input>
             </div>
             <div className="form-label">
               <h2>Fun Fact</h2>
-              
-              <textarea placeholder="Fun Fact"></textarea>
+              <h3>{this.state.factError}</h3>
+              <textarea placeholder="Fun Fact" value={this.state.fact} onChange={this.editFact}></textarea>
             </div>
-            <button className="submit-button">Submit</button>
+            <button className="submit-button" onClick={this.submit}>Submit</button>
           </div>
         </div>
         <div className="half">
